5789cdb316c3df693abb7ebb23784d71caba7084
MarcoSavaglia <marco.savaglia@live.com.au>
2019-10-04 12:43:53 +0930

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/happy-co/protoc-gen-twirp_dart/generator"
+	"github.com/apptreesoftware/protoc-gen-twirp_dart/generator"
 	"github.com/gogo/protobuf/protoc-gen-gogo/plugin"
 	"io"
 	"io/ioutil"
